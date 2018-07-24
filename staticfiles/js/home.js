94f914dc8e9867793be684c2fa0aca9ce24c4220
MSI\hanse <hans.engstad@teknologiporten.no>
2018-07-24 13:29:36 +0200

    Worked on daterangepicker for cabin search. Working, but unable to change selected month.

--- staticfiles/js/home.js
+++ staticfiles/js/home.js
@@ -1,83 +1,59 @@
 
+var start = new Date();
+start.setDate(start.getDate() + 1);
 
-$('#daterange-picker').datepicker({
-    format: 'dd.mm.yy',
-    startDate: '+2d',
-    weekStart: 1,
-    maxViewMode: 0,
-    orientation: 'bottom',
-    container: '#daterange-picker'
+var end = new Date();
+end.setDate(end.getDate() + 2);
+
+var maxDate = new Date();
+maxDate.setFullYear(maxDate.getFullYear() + 1);
+
+var startString = start.getDate() + "." + start.getMonth() + "." + start.getFullYear().toString().substr(-2);
+var endString = end.getDate() + "." + end.getMonth() + "." + end.getFullYear().toString().substr(-2);
+
+$('#daterange-picker').daterangepicker({
+    // autoApply: true,
+    opens: 'center',
+    parentEl: "#daterange-picker",
+    locale: {
+    	format: 'DD.MM.YY'
+    },
+    startDate: start,
+    endDate: end,
+    minDate: start,
+    maxDate: maxDate,
+}, function(start, end, label) {
+  console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
 });
 
+$('#from-date-field').val(startString);
+$('#to-date-field').val(endString);
 
-/*//FROM DATE
-$('#from-date-field').datepicker({
-    format: 'dd.mm.yy',
-    startDate: '+1d',
-    weekStart: 1,
-    maxViewMode: 0,
-    orientation: 'bottom',
-    container:'#from-date'
-});
-
-$('#from-date-field').datepicker().on('show', function(e){ onDatepickerShow(e.date, '#from-date-field'); });
-$('#from-date-field').datepicker().on('changeDate', function(e){ onDatepickerChange(e.date, '#from-date-field-form'); });
-$('#from-date-field').datepicker().on('hide', function(e){ onDatepickerHide(e.date, '#from-date-field'); });
-
-$( "#from-date" ).click(function() {
-	$('#from-date-field').data("datepicker").show();
-});
-
-//TO DATE
-$('#to-date-field').datepicker({
-    format: 'dd.mm.yy',
-    startDate: '+2d',
-    weekStart: 1,
-    maxViewMode: 0,
-    orientation: 'bottom',
-    container: '#to-date'
-});
-
-$('#to-date-field').datepicker().on('show', function(e){ onDatepickerShow(e.date, '#to-date-field'); });
-$('#to-date-field').datepicker().on('changeDate', function(e){ onDatepickerChange(e.date, '#to-date-field-form'); });
-$('#to-date-field').datepicker().on('hide', function(e){ onDatepickerHide(e.date, '#to-date-field'); });
-
-$( "#to-date" ).click(function() {
-	$('#to-date-field').data("datepicker").show();
-});
-
-*/
-
-
-$('#persons-input').click(function(){
-	onPersonsClick();
-});
-$('#persons-header').click(function(){
-	onPersonsClick();
-});
 
 function setup(){
-	// datepickersSetup();
-	// personAmountSetup();
+	datepickersSetup();
 }
 
 function datepickersSetup(){
 
+	console.log("Setup");
+
 	var date = new Date();	//Current date
 
+	console.log(date);
+
 	date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);	//Tomorrow
-	var dateString_from = convertDateToDisplay_num(date);
+	console.log(date);
+	var from_date = date;
 
 	date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);	//Day after tomorrow
-	var dateString_to = convertDateToDisplay_num(date);
+	var to_date = date;
 	
-	//Set visual fields
-	$('#from-date-field').attr("placeholder", dateString_from);
-	$('#to-date-field').attr("placeholder", dateString_to);
 
-	//Set form fields
-	$('#from-date-field-form').val(dateString_from);
-	$('#to-date-field-form').val(dateString_to);
+
+	//Set visual fields
+	// $('#from-date-field').val(from_date.getDate() + "." + from_date.getMonth() + "." + from_date.getYear());
+	// $('#to-date-field').val(to_date.getDate() + "." + to_date.getMonth() + "." + to_date.getYear());
 }
 
 function personAmountSetup(){
@@ -91,102 +67,6 @@ function onPersonsClick(){
 		$(id).val("1");
 	}
 	$(id).select();
-
-	//Show popover
-	//$(id).attr("data-content", "heisann")
-	//$(id).popover();
 }
-
-function onDatepickerShow(date, field_id){
-	
-}
-
-function onDatepickerHide(date, field_id){
-	//field value will override date on datepicker
-	var fieldValue = $(field_id).val();
-}
-
-function onDatepickerChange(date, form_id){
-	//Update form fields
-	$(form_id).val(convertDateToDisplay_num(date));
-}
-
-
-
-
-
-function updateDateTextFieldDisplayed(id, dateString){
-	$(id).val(dateString);
-}
-
-function convertDateToDisplay_text(date){
-	var daysOfWeek = ["søn", "man", "tir", "ons", "tor", "fre", "lør"];
-	var months = ["jan", "feb", "mar", "apr", "mai", "jun", "jul", "aug", "sep", "okt", "nov", "des"];
-
-	var dateString = daysOfWeek[date.getDay()] + ". " + date.getDate() + "." + months[date.getMonth()];
-
-	return dateString;
-}
-
-function convertDateToDisplay_num(date){
-	var day = date.getDate();
-	day = day < 10 ? "0" + day : day;
-
-	var month = date.getMonth() + 1;
-	month = month < 10 ? "0" + month : month;
-
-	var year = date.getFullYear().toString().substr(2, 2);
-
-	var dateString = day + "." + month + "." + year;
-
-	return dateString;
-}
-
-function convertDateNumToDate(dateNum){
-	if(dateNum.length != 8)
-	{
-		return null;
-	}
-
-	var currentDate = new Date();
-	var yearStart = currentDate.getFullYear().toString().substr(0, 2);	//two first numbers in year. 2018 -> 20
-
-	var day = dateNum.substr(0, 2);
-	var month = dateNum.substr(3, 2);
-	var year =  yearStart + dateNum.substr(6, 2);
-
-	try{
-		var monthIndex = parseInt(month) - 1;
-		var chosenDate = new Date(year, monthIndex, day);
-
-		return chosenDate;
-
-	} catch(err) {
-		console.log(err);
-		return null;
-	}
-
-	
-
-
-
-}
-
-function submitSearch(){
-	if($('#persons-input').val() == "")
-	{
-		$('#person-amount-form').val("1");
-	}
-	else
-	{
-		$('#person-amount-form').val($('#persons-input').val());
-	}
-
-
-
-	
-	$('#search-cabin-form').submit();
-}
-
 
 setup();
