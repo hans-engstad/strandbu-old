e0c373f58092ef7319031d204c89d197ac1c4d3b
MSI\hanse <hans.engstad@teknologiporten.no>
2018-07-24 17:07:01 +0200

    Now using two seperate daterangepickers to pick cabin search dates. Added js logic to prevent unvalid dates beeing chosen. Seems to be working.

--- staticfiles/js/home.js
+++ staticfiles/js/home.js
@@ -11,23 +11,65 @@ maxDate.setFullYear(maxDate.getFullYear() + 1);
 var startString = start.getDate() + "." + start.getMonth() + "." + start.getFullYear().toString().substr(-2);
 var endString = end.getDate() + "." + end.getMonth() + "." + end.getFullYear().toString().substr(-2);
 
-$('#daterange-picker').daterangepicker({
-    // autoApply: true,
-    opens: 'center',
-    parentEl: "#daterange-picker",
-    locale: {
-    	format: 'DD.MM.YY'
-    },
-    startDate: start,
-    endDate: end,
-    minDate: start,
-    maxDate: maxDate,
-}, function(start, end, label) {
-  console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+$(function() {
+	$('#daterange-from-date').daterangepicker({
+		singleDatePicker: true,
+		opens: 'center',
+		locale: {
+			format: 'DD.MM.YY'
+		},
+		minDate: start,
+		startDate: start,
+		}, 
+		function(start, end, label) {
+			//NOTE: start and end parameters are the same
+			start = new Date(start);
+			start.setHours(0,0,0,0);
+			//Check that to-date is after this date
+			end = $('#daterange-to-date').data('startDate');
+			end.setHours(0,0,0,0);
+			if(start >= end)
+			{
+				//Set to-date after from-date
+				end = new Date(start.setDate(start.getDate() + 1));
+				$('#daterange-to-date').data('daterangepicker').setStartDate(end);
+				$('#daterange-to-date').data('daterangepicker').setEndDate(end);
+			}
+
+		}
+	);
 });
 
-$('#from-date-field').val(startString);
-$('#to-date-field').val(endString);
+$(function() {
+	$('#daterange-to-date').daterangepicker({
+		singleDatePicker: true,
+		opens: 'center',
+		locale: {
+			format: 'DD.MM.YY'
+		},
+		minDate: end,
+		startDate: end,
+		}, 
+		function(start, end, label) {
+			//NOTE: start and end parameters are the same
+			end = new Date(end);
+			end.setHours(0,0,0,0);
+			//Check that to-date is after this date
+			_start = moment($('#daterange-from-date').val(), 'DD.MM.YY').toDate();
+			_start.setHours(0,0,0,0);
+			if(_start >= end)
+			{
+				//Set from-date before to-date
+				start = new Date(end.setDate(end.getDate() - 1));
+				$('#daterange-from-date').data('daterangepicker').setStartDate(start);
+				$('#daterange-from-date').data('daterangepicker').setEndDate(start);
+			}
+		}
+	);
+});
+
+$('#daterange-from-date').val(startString);
+$('#daterange-to-date').val(endString);
 
 
 function setup(){
